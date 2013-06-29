d0043b8876d6b690b12747764856db9b69f42dff
vijay.devadass@gmail.com <vijay.devadass@gmail.com@1390098f-01a9-0b94-8680-04a7a358d0c8>
2013-06-29 07:55:40 +0000

    Adding CSVParser

---
+++ IndianBridge/CSVParser/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CSVParser
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.btnSelectFile = new System.Windows.Forms.Button();
+			this.lblContents = new System.Windows.Forms.Label();
+			this.txtFileContents = new System.Windows.Forms.TextBox();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.txtResults = new System.Windows.Forms.TextBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.btnSaveHtml = new System.Windows.Forms.Button();
+			this.txtTitle = new System.Windows.Forms.TextBox();
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.txtFileName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPath = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtDomainName = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnSelectFile
+			// 
+			this.btnSelectFile.Location = new System.Drawing.Point(39, 22);
+			this.btnSelectFile.Name = "btnSelectFile";
+			this.btnSelectFile.Size = new System.Drawing.Size(96, 32);
+			this.btnSelectFile.TabIndex = 0;
+			this.btnSelectFile.Text = "Select CSV File";
+			this.btnSelectFile.UseVisualStyleBackColor = true;
+			this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+			// 
+			// lblContents
+			// 
+			this.lblContents.AutoSize = true;
+			this.lblContents.Location = new System.Drawing.Point(370, 34);
+			this.lblContents.Name = "lblContents";
+			this.lblContents.Size = new System.Drawing.Size(45, 13);
+			this.lblContents.TabIndex = 3;
+			this.lblContents.Text = "Preview";
+			// 
+			// txtFileContents
+			// 
+			this.txtFileContents.Location = new System.Drawing.Point(373, 70);
+			this.txtFileContents.Multiline = true;
+			this.txtFileContents.Name = "txtFileContents";
+			this.txtFileContents.Size = new System.Drawing.Size(507, 433);
+			this.txtFileContents.TabIndex = 2;
+			// 
+			// txtResults
+			// 
+			this.txtResults.Location = new System.Drawing.Point(39, 283);
+			this.txtResults.Multiline = true;
+			this.txtResults.Name = "txtResults";
+			this.txtResults.Size = new System.Drawing.Size(294, 216);
+			this.txtResults.TabIndex = 4;
+			this.txtResults.Visible = false;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(157, 36);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(109, 17);
+			this.checkBox1.TabIndex = 5;
+			this.checkBox1.Text = "Show Debug Info";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// btnSaveHtml
+			// 
+			this.btnSaveHtml.Location = new System.Drawing.Point(39, 244);
+			this.btnSaveHtml.Name = "btnSaveHtml";
+			this.btnSaveHtml.Size = new System.Drawing.Size(96, 32);
+			this.btnSaveHtml.TabIndex = 6;
+			this.btnSaveHtml.Text = "Save HTML";
+			this.btnSaveHtml.UseVisualStyleBackColor = true;
+			this.btnSaveHtml.Visible = false;
+			this.btnSaveHtml.Click += new System.EventHandler(this.btnSaveHtml_Click);
+			// 
+			// txtTitle
+			// 
+			this.txtTitle.Location = new System.Drawing.Point(39, 90);
+			this.txtTitle.Name = "txtTitle";
+			this.txtTitle.Size = new System.Drawing.Size(192, 20);
+			this.txtTitle.TabIndex = 7;
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Location = new System.Drawing.Point(36, 74);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(112, 13);
+			this.lblTitle.TabIndex = 8;
+			this.lblTitle.Text = "HTML Document Title";
+			// 
+			// txtFileName
+			// 
+			this.txtFileName.Location = new System.Drawing.Point(39, 133);
+			this.txtFileName.Name = "txtFileName";
+			this.txtFileName.Size = new System.Drawing.Size(192, 20);
+			this.txtFileName.TabIndex = 9;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(36, 115);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(47, 13);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Save As";
+			// 
+			// txtPath
+			// 
+			this.txtPath.Location = new System.Drawing.Point(39, 218);
+			this.txtPath.Name = "txtPath";
+			this.txtPath.Size = new System.Drawing.Size(192, 20);
+			this.txtPath.TabIndex = 11;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(36, 202);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(86, 13);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "Path on web site";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(250, 90);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(85, 162);
+			this.button1.TabIndex = 13;
+			this.button1.Text = "Publish";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(36, 161);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 13);
+			this.label3.TabIndex = 15;
+			this.label3.Text = "Domain";
+			// 
+			// txtDomainName
+			// 
+			this.txtDomainName.Location = new System.Drawing.Point(39, 177);
+			this.txtDomainName.Name = "txtDomainName";
+			this.txtDomainName.Size = new System.Drawing.Size(192, 20);
+			this.txtDomainName.TabIndex = 14;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(959, 509);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtDomainName);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtPath);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtFileName);
+			this.Controls.Add(this.lblTitle);
+			this.Controls.Add(this.txtTitle);
+			this.Controls.Add(this.btnSaveHtml);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.txtResults);
+			this.Controls.Add(this.lblContents);
+			this.Controls.Add(this.txtFileContents);
+			this.Controls.Add(this.btnSelectFile);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button btnSelectFile;
+		private System.Windows.Forms.Label lblContents;
+		private System.Windows.Forms.TextBox txtFileContents;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.TextBox txtResults;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button btnSaveHtml;
+		private System.Windows.Forms.TextBox txtTitle;
+		private System.Windows.Forms.Label lblTitle;
+		private System.Windows.Forms.TextBox txtFileName;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPath;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtDomainName;
+	}
+}
+
---
+++ IndianBridge/CSVParser/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using IndianBridge.Common;
+using IndianBridge.WordpressAPIs;
+
+namespace CSVParser
+{
+	public partial class Form1 : Form
+	{
+		private Tuple<DataTable, string[]> m_parseResults;
+		private CustomBackgroundWorker m_publishResultsCBW = null;
+		private bool m_publishResultsRunning = false;
+		private double oldFontSize;
+
+		public Form1()
+		{
+			InitializeComponent();
+			this.folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer;
+			txtFileContents.Visible = lblContents.Visible = btnSaveHtml.Visible = txtTitle.Visible = lblTitle.Visible = false;
+		}
+
+		private void btnSelectFile_Click(object sender, EventArgs e)
+		{
+			var size = -1;
+			var fileContents = String.Empty;
+			// Show the dialog and get result.
+			var result = openFileDialog1.ShowDialog();
+			if (result == DialogResult.OK) // Test result.
+			{
+				var file = openFileDialog1.FileName;
+				try
+				{
+					fileContents = File.ReadAllText(file);
+					size = fileContents.Length;
+				}
+				catch (Exception exc)
+				{
+					txtResults.AppendText(exc.Message);
+					txtResults.Visible = true;
+				}
+			}
+
+			txtResults.AppendText("File size is " + size + "\n");
+			txtResults.AppendText(result.ToString());
+
+			m_parseResults = ParseCSV(fileContents);
+
+			txtFileContents.Visible = lblContents.Visible = btnSaveHtml.Visible = txtTitle.Visible = lblTitle.Visible = true;
+		}
+
+		private Tuple<DataTable, string[]> ParseCSV(string fileContents)
+		{
+			Tuple<DataTable, string[]> parseResults = null;
+			try
+			{
+				// Tracks the position within the file
+				var previousPosition = 0;
+
+				// Skip empty lines at the beginning of the file, if any
+				var results = SkipEmptyLines(fileContents);
+
+				var currentPosition = results.IndexOf("\n");
+				var headerLine = results.Substring(previousPosition, currentPosition - previousPosition);
+
+				var columnNames = GetColumns(headerLine);
+				var values = ConstructDataTable(columnNames);
+
+				txtResults.AppendText("\n");
+				txtResults.AppendText("Columns: ");
+				var columnsOutput = columnNames.Aggregate(String.Empty,
+				                                          (current, column) => current + String.Format("{0} ,", column));
+
+				columnsOutput = columnsOutput.TrimEnd(new[] {','});
+				txtResults.AppendText(columnsOutput);
+
+				// Skip empty lines after the header, if any
+				results = SkipEmptyLines(results.Substring(currentPosition + 1));
+
+				//txtFileContents.AppendText(results);
+
+				previousPosition = 0;
+				currentPosition = results.IndexOf("\n");
+
+				while (true)
+				{
+					var currentLine = results.Substring(previousPosition, currentPosition - previousPosition);
+					if (currentLine == String.Empty || currentLine == "\r") break;
+					var rowValues = currentLine.Split(new[] {','});
+					AddRow(rowValues, columnNames, ref values);
+					previousPosition = currentPosition + 1;
+					currentPosition = results.IndexOf("\n", previousPosition);
+					if (currentPosition < 0) break;
+				}
+
+				// Show the preview
+				PublishResults(values);
+
+				parseResults = new Tuple<DataTable, string[]>(values, columnNames);
+			}
+			catch (Exception exc)
+			{
+				txtResults.AppendText("\n");
+				txtResults.AppendText(exc.Message);
+				txtResults.AppendText("\n");
+				txtResults.AppendText(exc.StackTrace);
+			}
+
+			return parseResults;
+		}
+
+		private void PublishResults(DataTable results)
+		{
+			foreach (DataRow row in results.Rows)
+			{
+				WriteRowResult(row.ItemArray);
+			}
+		}
+
+		private void PublishResultsToHTML(DataTable results, IEnumerable<string> columnNames, string fileName = @"C:\results.html")
+		{
+			var htmlHeader = String.Format("<html><h2>{0}</h2><table border='1'>\n", txtTitle.Text);
+			var htmlString = htmlHeader  + GetHTMLTableHeader(columnNames);
+			htmlString = results.Rows.Cast<DataRow>().Aggregate(htmlString, (current, row) => 
+				current + GetHTMLRowResult(row.ItemArray));
+			htmlString += "\n</table></html>";
+			WriteFile(fileName, htmlString);
+		}
+
+		private static void WriteFile(string fileName, string content)
+		{
+			var fileStream = new StreamWriter(fileName);
+			fileStream.Write(content);
+			fileStream.Close();
+		}
+
+		private static string GetHTMLTableHeader(IEnumerable<object> columnNames)
+		{
+			var tableHeader = columnNames.Aggregate("<tr>", (current, value) =>
+				current + String.Format("<th>{0}</th>", value.ToString().Trim()));
+			tableHeader += "</tr>\n";
+			return tableHeader;
+		}
+
+		private string GetHTMLRowResult(IEnumerable<object> rowValues)
+		{
+			var rowString = rowValues.Aggregate("<tr>", (current, value) => 
+				current + String.Format("<td>{0}</td>", value.ToString().Trim()));
+			rowString += "</tr>\n";
+			return rowString;
+		}
+
+		private void WriteRowResult(IEnumerable<object> rowValues)
+		{
+			var rowString = rowValues.Aggregate(String.Empty, (current, rowValue) => current + String.Format("{0},", rowValue.ToString().Trim()));
+			rowString = rowString.TrimEnd(new[] { ',' });
+			rowString = rowString.Replace("\r", String.Empty);
+			txtFileContents.AppendText(rowString + "\n");
+		}
+
+		private static DataTable ConstructDataTable(IEnumerable<string> columnNames)
+		{
+			var result = new DataTable();
+			foreach (var columnName in columnNames)
+			{
+				result.Columns.Add(columnName, Type.GetType("System.String"));
+			}
+			return result;
+		}
+
+		private void AddRow(string[] rowValues, string[] columnNames, ref DataTable result)
+		{
+			var row = result.NewRow();
+			for (var i = 0; i < columnNames.Length; i++ )
+			{
+				row[columnNames[i]] = rowValues[i];
+			}
+			result.Rows.Add(row);
+		}
+
+		private string[] GetColumns(string headerLine)
+		{
+			return headerLine.Split(new char[] { ',' });
+		}
+
+		private string SkipEmptyLines(string contents)
+		{
+			var previousPosition = 0;
+			var currentPosition = contents.IndexOf("\n");
+			var line = contents.Substring(previousPosition, currentPosition - previousPosition);
+			while (true)
+			{
+				if (line != "\r")
+				{
+					break;
+				}
+
+				previousPosition = currentPosition + 1;
+				currentPosition = contents.IndexOf("\n", previousPosition);
+				line = contents.Substring(previousPosition, currentPosition - previousPosition);
+			}
+
+			return contents.Substring(previousPosition);
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked)
+				checkBox1.Text = "Hide debug info";
+			txtResults.Visible = checkBox1.Checked;
+		}
+
+		private void btnSaveHtml_Click(object sender, EventArgs e)
+		{
+			if (m_parseResults == null)
+			{
+				txtResults.AppendText("No results found - please check the input file");
+				return;
+			}
+
+			var result = folderBrowserDialog1.ShowDialog();
+			if (result != DialogResult.OK) return;
+
+			var foldername = folderBrowserDialog1.SelectedPath;
+			PublishResultsToHTML(m_parseResults.Item1, m_parseResults.Item2, String.Format(@"{0}\results.html", foldername));
+
+			MessageBox.Show("HTML File (results.html) saved successfully", "Success");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string siteName = "http://bfi.net.in/bfitest";
+			string pagePath = "/tourneys/winter-national/y2012/results/team-event";
+			string username = "vdevadass";
+			string password = "bitspilani";
+			UploadWebpages uw = new UploadWebpages(siteName, username, password, true);
+			var m_publishResultsCBW = new CustomBackgroundWorker("Publish Results", uw.uploadDirectoryInBackground, publishResultsCompleted, null, null, null, null);
+			oldFontSize = Utilities.fontSize;
+			Utilities.fontSize = 5;
+			var values = new Tuple<string, string>(folderBrowserDialog1.SelectedPath, pagePath);
+			m_publishResultsRunning = true;
+			m_publishResultsCBW.run(values);
+			MessageBox.Show("Published successfully", "Success");
+
+		}
+
+		private void publishResultsCompleted(bool success)
+		{
+			m_publishResultsRunning = false;
+			Utilities.fontSize = oldFontSize;
+			if (success) txtResults.Text = ("Results published succesfully at " + txtDomainName.Text);
+		}
+	}
+}
